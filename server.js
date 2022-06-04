e08b5e094ffb688dbdcacdef29c4585ca62e738c
qyzhencharles <qyzhencharles@gmail.com>
2022-06-04 17:42:24 -0400

    update

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,3 +19,24 @@ app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
+
+// Add a GET route that returns the projectData object
+app.get('/projectData', (req, res)=>{
+    res.send(projectData);
+})
+
+// Add a POST route that adds incoming data to projectData
+app.post('/addData', (req, res)=>{
+    const newData = req.body;
+    const newEntry = {
+        temperature: newData.temperature,
+        date: newData.date,
+        userReponse: newData.userReponse
+    }
+    projectData.push(newEntry);
+})
+
+// Add a POST route 
--- website/app.js
+++ website/app.js
@@ -1,5 +1,32 @@
 /* Global Variables */
-
+const baseURL = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=';
+const apiKey = '1f12b2df013448113e58acf16943d12f';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Make a GET request
+const getWeather = async (url, zip, apiKey) => {
+    const res = await fetch(url+zip+apiKey);
+    try {
+        const newData = await res.json();
+        console.log(newData);
+
+    } catch {
+        console.log(error);
+    }
+}
+
+// Make a post request
+const postWeather = async (url, zip, )
+
+// Make a event listener
+document.getElementById('generate').addEventListener('click', performAction);
+
+const performAction = () => {
+    const zip = document.getElementById('zip').value;
+    getWeather(baseURL, zip, apiKey)
+    .then(data=>{
+        console.log(data);
+    });
+}
